410fcc6fbf84905c19c1d183107b5066340f1405
vibovenkat123 <vibovenkat@gmail.com>
2022-09-01 20:07:05 -0500

    made code cleaner made array already have 1 and two

--- fib.js
+++ fib.js
@@ -1,19 +1,14 @@
 function fibs(n) {
-  let newArray = [];
-  for (let i = 0; i < n; i++) {
+  let newArray = [0, 1];
+  for (let i = 2; i < n; i++) {
     if (newArray.length == 0) {
       newArray.push(0)
     }
     else {
-      console.log(newArray[i - 1])
       let num = newArray[i - 1] + newArray[i - 2];
-      if (newArray[i - 2] == undefined) {
-        newArray.push(i)
-      }
-      else {
-        newArray.push(num)
-      }
+      newArray.push(num)
     }
   }
   return newArray;
 }
+console.log(fibs(8))
