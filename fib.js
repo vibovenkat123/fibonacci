ab446b10d184a0db6f9030225cf107c1bf80d69d
vibovenkat123 <vibovenkat@gmail.com>
2022-09-01 20:46:23 -0500

    add fibsRec function which uses recursion

--- fib.js
+++ fib.js
@@ -1,14 +1,15 @@
 function fibs(n) {
   let newArray = [0, 1];
   for (let i = 2; i < n; i++) {
-    if (newArray.length == 0) {
-      newArray.push(0)
-    }
-    else {
-      let num = newArray[i - 1] + newArray[i - 2];
-      newArray.push(num)
-    }
+    newArray.push(newArray[i - 1] + newArray[i - 2])
   }
   return newArray;
 }
-console.log(fibs(8))
+function fibsRec(n, array = [0, 1]) {
+  if (array.length >= n) {
+    return array;
+  }
+
+  array.push(array.at(-1) + array.at(-2));
+  return fibsRec(n, array);
+}
