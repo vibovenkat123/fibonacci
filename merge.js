97a302fc584507b73670c881cfe8288fc2954291
vibovenkat123 <vibovenkat@gmail.com>
2022-09-02 21:05:41 -0500

    make variables cleaner

--- merge.js
+++ merge.js
@@ -1,9 +1,8 @@
 function mergeSort(arr) {
-  // get the middle of the array
-  let middle = arr.length / 2;
-  middle = Math.trunc(middle)
-  // get the left side
-  let leftSide = arr.slice(0, middle)
-  // get the right side
-  let rightSide = arr.slice(middle)
+  if (arr.length <= 1) {
+    return arr;
+  }
+  let middle = Math.floor(arr.length / 2);
+  let leftSide = mergeSort(arr.slice(0, middle));
+  let rightSide = mergeSort(arr.slice(middle))
 }
