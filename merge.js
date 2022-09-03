04fef65b148ce0ce7e88559abe3895db7766e51a
vibovenkat123 <vibovenkat@gmail.com>
2022-09-02 20:38:26 -0500

    made function to get right and left side

---
+++ merge.js
@@ -0,0 +1,9 @@
+function mergeSort(arr) {
+  // get the middle of the array
+  let middle = arr.length / 2;
+  middle = Math.trunc(middle)
+  // get the left side
+  let leftSide = arr.slice(0, middle)
+  // get the right side
+  let rightSide = arr.slice(middle)
+}
